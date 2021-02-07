d2b870f0fb5a17b440b49308649cc34f0e708d3c
Aji <ditaajipratama@protonmail.com>
2021-02-07 14:04:16 +0700

    Add icon in battery status

--- battery.sh
+++ battery.sh
@@ -1,1 +1,31 @@
-echo $(cat /sys/class/power_supply/BAT0/status) $(cat /sys/class/power_supply/BAT0/capacity)"%"
+#!/bin/sh
+a=""
+b=""
+
+status=$(cat /sys/class/power_supply/BAT0/status)
+if [ $status = "Charging" ]; then
+    a=""
+elif [ $status = "Discharging" ]; then
+    a=""
+elif [ $status = "Not Charging" ]; then
+    a=""
+else
+    a=" Error"
+fi
+
+cap=$(cat /sys/class/power_supply/BAT0/capacity)
+if [ $cap -ge 0 ] && [ $cap -le 20 ]; then
+    b=""
+elif [ $cap -ge 21 ] && [ $cap -le 40 ]; then
+    b=""
+elif [ $cap -ge 41 ] && [ $cap -le 60 ]; then
+    b=""
+elif [ $cap -ge 61 ] && [ $cap -le 90 ]; then
+    b=""
+elif [ $cap -ge 91 ] && [ $cap -le 100 ]; then
+    b=""
+else
+    b=""
+fi
+
+echo " "$a $b $cap"%"
