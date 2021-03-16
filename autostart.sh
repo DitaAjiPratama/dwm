9d4b705d1358bab64fd891a5e4c5e4730d9030ef
Dita Aji Pratama <aji@spike>
2021-03-16 19:24:44 +0700

    Don't need to thread sleep (Maybe?)

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,6 @@
 feh --bg-scale ~/dwm/wallpaper/8913281_p0.jpg
 
 while true; do
-        xsetroot -name "  $(sh ./volume.sh) | $(sh ./battery.sh) | $(date +'%F %R') "
-    sleep 1
+  xsetroot -name "  $(sh ./volume.sh) | $(sh ./battery.sh) | $(date +'%F %R') |  "
 done
 
