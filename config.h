4c5f069bef04742165beafd06261c59e5b52468f
Dita Aji Pratama <ditaajipratama@protonmail.com>
2021-06-25 03:04:56 +0700

    Latest config.h

--- config.h
+++ config.h
@@ -1,7 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-#include <X11/XF86keysym.h>
-
 static const unsigned int borderpx  = 2;
 static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;
@@ -16,10 +14,15 @@ static const char col_yellow1[]     = "#F5A201";
 static const char col_yellow2[]     = "#FFBA42";
 static const char col_blush[]       = "#F8E5EE";
 
+static const char col_dark1[]       = "#162736";
+static const char col_dark2[]       = "#203449";
+static const char col_dark3[]       = "#708594";
+static const char col_white[]       = "#FFFFFF";
+
 static const char *colors[][3]      = {
-	/* fg, bg, border */
-	[SchemeNorm] = { col_blush, col_blue1,   col_blue1   },
-	[SchemeSel]  = { col_blue1, col_yellow1, col_yellow2 },
+	/*               fg,        bg,        border    */
+	[SchemeNorm] = { col_white, col_dark1, col_dark1 },
+	[SchemeSel ] = { col_white, col_dark3, col_blush },
 };
 
 static const char *tags[] = { "1", "2", "3", "4", "5" };
@@ -35,14 +38,14 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact       = 0.55; /* factor of master area size [0.05..0.95] */
+static const int   nmaster     = 1;    /* number of clients in master area */
+static const int   resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile    },    /* first entry is default */
+	{ "><>",      NULL    },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -58,62 +61,73 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static       char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark1, "-nf", col_white, "-sb", col_dark3, "-sf", col_white, NULL };
 
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_blue2, "-nf", col_blush, "-sb", col_yellow2, "-sf", col_blue1, NULL };
+static const char *termcmd[]  = { "konsole"	, NULL };
+static const char *filecmd[]  = { "thunar"	, NULL };
 
-static const char *termcmd[]  = { "konsole", NULL };
-static const char *filecmd[]  = { "thunar", NULL };
+static const char *scrotcmd[] = { "scrot"		, NULL };
 
-static const char *scrotcmd[]	= { "scrot", "-t", "25", NULL };
+/* static const char *nmtui[]    = { "st"       , "nmtui" , NULL }; */
+static const char *nmtui[]    = { "st"			, "nmtui"			, NULL };
+static const char *nmcedit[]	= { "nm-connection-editor"	, NULL };
+static const char *htop[]     = { "st"			, "htop"			, NULL };
 
-static const char *volup[]		= { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
-static const char *voldown[]  = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *volup[]    = { "pactl"		, "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *voldown[]  = { "pactl"		, "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
 
 #include "shiftview.c"
 #include "shifttag.c"
+#include <X11/XF86keysym.h>
 
 static Key keys[] = {
-	/* modifier                     key												function        argument */
-  { 0,														XF86XK_AudioLowerVolume,	spawn,          {.v = voldown } },
-  { 0,														XF86XK_AudioRaiseVolume,	spawn,          {.v = volup } },
+	/* modifier											key												function		argument */
+	{ 0,														XF86XK_AudioLowerVolume,	spawn,			{.v = voldown}	},
+	{ 0,														XF86XK_AudioRaiseVolume,	spawn,			{.v = volup}		},
+	{ MODKEY,												XK_comma,									spawn,			{.v = voldown}	},
+	{ MODKEY,												XK_period,								spawn,			{.v = volup}		},
 
-	{ MODKEY,                       XK_p,											spawn,          {.v = dmenucmd } },
-  { MODKEY,                       XK_k,											spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_e,											spawn,          {.v = filecmd } },
+	{ MODKEY,												XK_p,											spawn,			{.v = dmenucmd}	},
+	{ MODKEY,												XK_k,											spawn,			{.v = termcmd}	},
+	{ MODKEY,												XK_e,											spawn,			{.v = filecmd}	},
 
-	{ 0,														XK_Print,									spawn,      	  {.v = scrotcmd } },
-  { MODKEY,                       XK_b,											togglebar,      {0} },
+	{ MODKEY,												XK_n,											spawn,			{.v = nmtui}		},
+	{ MODKEY|ShiftMask,							XK_n,											spawn,			{.v = nmcedit}	},
+	{ MODKEY,												XK_h,											spawn,			{.v = htop}			},
 
-	{ MODKEY,                       XK_minus,									setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,									setgaps,        {.i = +1 } },
+	{ 0,														XK_Print,									spawn,			{.v = scrotcmd}	},
+	{ MODKEY,												XK_b,											togglebar,	{0}							},
 
-  { MODKEY,                       XK_Prior,									focusstack,     {.i = -1 } },
-  { MODKEY,                       XK_Next,									focusstack,     {.i = +1 } },
-  { MODKEY,                       XK_Return,								zoom,           {0} },
+	{ MODKEY,												XK_minus,									setgaps,		{.i = -1}				},
+	{ MODKEY,												XK_equal,									setgaps,		{.i = +1}				},
 
-  { MODKEY|ControlMask,           XK_Left,									shiftview,      {.i = -1 } },
-  { MODKEY|ControlMask,           XK_Right,									shiftview,      {.i = +1 } },
+	{ MODKEY,												XK_Prior,									focusstack,	{.i = -1}				},
+	{ MODKEY,												XK_Next,									focusstack,	{.i = +1}				},
+	{ MODKEY,												XK_Return,								zoom,				{0}							},
 
-	{ MODKEY|ShiftMask,							XK_Tab,										shiftview,      {.i = -1 } },
-  { MODKEY,												XK_Tab,										shiftview,      {.i = +1 } },
+	{ MODKEY|ControlMask,						XK_Left,									shiftview,	{.i = -1}				},
+	{ MODKEY|ControlMask,						XK_Right,									shiftview,	{.i = +1}				},
 
-	TAGKEYS(XK_1,	0)
-  TAGKEYS(XK_2,	1)
-  TAGKEYS(XK_3,	2)
-  TAGKEYS(XK_4,	3)
-  TAGKEYS(XK_5,	4)
+	{ MODKEY|ShiftMask,							XK_Tab,										shiftview,	{.i = -1}				},
+	{ MODKEY,												XK_Tab,										shiftview,	{.i = +1}				},
 
-	{ MODKEY|ControlMask|ShiftMask, XK_Left,									shifttag,       {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_Right,									shifttag,       {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,	XK_Left,									shifttag,		{.i = -1}				},
+	{ MODKEY|ControlMask|ShiftMask,	XK_Right,									shifttag,		{.i = +1}				},
 
-  { MODKEY,												XK_w,											killclient,     {0} },
-  { MODKEY|ShiftMask,							XK_q,											quit,           {0} },
+	{ MODKEY,												XK_w,											killclient,	{0}							},
+	{ MODKEY|ShiftMask,							XK_q,											quit,				{0}							},
 
-  { MODKEY|Mod1Mask,              XK_Up,										incnmaster,     {.i = +1 } },
-  { MODKEY|Mod1Mask,              XK_Down,									incnmaster,     {.i = -1 } },
-  { MODKEY|Mod1Mask,              XK_Left,									setmfact,       {.f = -0.05} },
-  { MODKEY|Mod1Mask,              XK_Right,									setmfact,       {.f = +0.05} },
+	{ MODKEY|Mod1Mask,							XK_Up,										incnmaster,	{.i = +1}				},
+	{ MODKEY|Mod1Mask,							XK_Down,									incnmaster,	{.i = -1}				},
+	{ MODKEY|Mod1Mask,							XK_Left,									setmfact,		{.f = -0.05}		},
+	{ MODKEY|Mod1Mask,							XK_Right,									setmfact,		{.f = +0.05}		},
+
+	TAGKEYS(XK_1, 0)
+	TAGKEYS(XK_2, 1)
+	TAGKEYS(XK_3, 2)
+	TAGKEYS(XK_4, 3)
+	TAGKEYS(XK_5, 4)
 };
 
 /* button definitions */
