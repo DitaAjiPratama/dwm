8883367fb45a100ccc0afeb2e0be91a3a945f8e8
Aji Laptop 3YK <aji@triyakom.com>
2021-11-05 21:21:29 +0700

    Remove volume comment

--- script/volume.sh
+++ script/volume.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-#status=$(
-#pactl list sinks | grep '^[[:space:]]Volume:' | \
-#    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
-#)
+
 status=$(
 awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)
 )
