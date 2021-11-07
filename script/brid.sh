4d112e4e8ed7e76242930c07c833ba6374407f29
Aji Laptop 3YK <aji@triyakom.com>
2021-11-07 13:34:08 +0700

    Add script to increase and decrease a brightness

---
+++ script/brid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+conn=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1)
+a=$(xrandr --verbose --current | grep ^$conn -A5 | tail -n1)
+curb="${a##* }"
+
+[ "$curb" == "1.0" ] && xrandr --output $conn --brightness 0.9
+[ "$curb" == "0.90" ] && xrandr --output $conn --brightness 0.8
+[ "$curb" == "0.80" ] && xrandr --output $conn --brightness 0.7
+[ "$curb" == "0.70" ] && xrandr --output $conn --brightness 0.6
+[ "$curb" == "0.60" ] && xrandr --output $conn --brightness 0.5
+[ "$curb" == "0.50" ] && xrandr --output $conn --brightness 0.4
+[ "$curb" == "0.40" ] && xrandr --output $conn --brightness 0.3
+[ "$curb" == "0.30" ] && xrandr --output $conn --brightness 0.2
+[ "$curb" == "0.20" ] && xrandr --output $conn --brightness 0.1
+
+echo $curb
