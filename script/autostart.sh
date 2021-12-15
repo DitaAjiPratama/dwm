87a1301fe68cac7d1afea09b542c282bc1a30767
Aji <ditaajipratama@protonmail.com>
2021-12-15 09:36:02 +0700

    disable display for battery status bar when not use battery

--- script/autostart.sh
+++ script/autostart.sh
@@ -1,7 +1,9 @@
-feh --bg-scale ~/.dwm/wallpaper/8913281_p0.jpg
+feh --bg-fill ~/Pictures/Wallpapers/saber-alter-two-handed-sword-blonde-dress-armor-fate-grand-order-transformation-29836-3840x2160.jpeg
+
+xrandr --output HDMI-2 --auto --left-of DP-1
 
 while true; do
-	xsetroot -name " $(bash ./brightness.sh) $(bash ./volume.sh) $(sh ./battery.sh) $(sh ./time.sh)  "
+	xsetroot -name " $(bash ./brightness.sh) $(bash ./volume.sh) $(sh ./battery.sh)$(sh ./time.sh)  "
 	#xsetroot -name " $(sh ./volume.sh) $(sh ./time.sh)  "
 	sleep 1
 done
--- script/battery.sh
+++ script/battery.sh
@@ -16,17 +16,20 @@ fi
 cap=$(cat /sys/class/power_supply/BAT0/capacity)
 if [ $cap -ge 0 ] && [ $cap -le 20 ]; then
     b=""
+    echo $a $b $cap "| "
 elif [ $cap -ge 21 ] && [ $cap -le 40 ]; then
     b=""
+    echo $a $b $cap "| "
 elif [ $cap -ge 41 ] && [ $cap -le 60 ]; then
     b=""
+    echo $a $b $cap "| "
 elif [ $cap -ge 61 ] && [ $cap -le 90 ]; then
     b=""
+    echo $a $b $cap "| "
 elif [ $cap -ge 91 ] && [ $cap -le 100 ]; then
     b=""
+    echo $a $b $cap "| "
 else
     b=""
 fi
-
-echo $a $b $cap "|"
 
