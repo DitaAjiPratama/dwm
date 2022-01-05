ed2c6a4d95b33b63ed2e13badaa27dca0ebfed6b
Aji <aji@triyakom.com>
2022-01-05 14:49:01 +0700

    from office 3yk

--- script/autostart.sh
+++ script/autostart.sh
@@ -1,10 +1,8 @@
-feh --bg-fill ~/Pictures/Wallpapers/saber-alter-two-handed-sword-blonde-dress-armor-fate-grand-order-transformation-29836-3840x2160.jpeg
-
-xrandr --output HDMI-2 --auto --left-of DP-1
+$(bash ./wallpaper.sh)
+$(bash ./display.sh)
 
 while true; do
-	xsetroot -name " $(bash ./brightness.sh) $(bash ./volume.sh) $(sh ./battery.sh)$(sh ./time.sh)  "
-	#xsetroot -name " $(sh ./volume.sh) $(sh ./time.sh)  "
+	xsetroot -name " $(bash ./brightness.sh) $(bash ./volume.sh) $(sh ./battery.sh)$(sh ./time.sh)  Aji DAP "
 	sleep 1
 done
 
---
+++ script/wallpaper.sh
@@ -0,0 +1,1 @@
+feh --bg-fill ~/Pictures/Wallpapers/daniel-tseng-W9kq9suABY4-unsplash.jpg
