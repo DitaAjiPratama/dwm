f75466a64422f71e14ab2b12dda9fee149788ef3
Dita Aji Pratama <ditaajipratama@protonmail.com>
2022-01-10 10:35:38 +0700

    Create install.sh

---
+++ script/install.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+
+sudo apt-get install -y git xorg libx11-dev libxft-dev libxinerama-dev xdm suckless-tools dmenu
+sudo apt-get install -y feh rofi kitty thunar scrot calcurse
+
+git clone https://github.com/DitaAjiPratama/dwm.git ~/.dwm
+
+cd ~/.dwm
+sudo make clean install
+
+echo "exec -dwm" > ~/.xsession
